f32417df86fbdd17e0d521009bc8b956c9e193da
Ilaletdinov Almaz <45946541+blablatdinov@users.noreply.github.com>
2024-12-25 11:18:41 +0300

    Update license year (#4)

--- entry.sh
+++ entry.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # MIT License
 #
-# Copyright (c) 2023 Almaz Ilaletdinov <a.Ilaletdinov@yandex.ru>
+# Copyright (c) 2023-2025 Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
